6d2703c075a26d4dc3526724900797e5eaeff849
Tony <thunter1987@yahoo.com>
2023-04-13 19:33:08 -0500

    "Tests Passed"

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "what does the this keyword mean?",
+  "What is the Constructor OO pattern?",
+  "implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What is NaN and how Can we Check for it",
+  "What is the difference between stopPropagation and preventDefault?",
+  "Immutable State and Pure Functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is JSONP?",
 ];
 
 const titleCased = () => {
-  return tutorials
+  return [...tutorials].map((tutorial) => {
+    const words = tutorial.split(' ').map((word) => {
+      return word.replace(word.at(0), word.charAt(0).toUpperCase());
+    });
+    return words.toString().replaceAll(',', ' ');
+  });
 }
+console.log(titleCased)
